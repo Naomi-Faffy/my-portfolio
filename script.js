bfe18d1c7b98c7b69c1cc125c0c1fafb1995dd60
Naomi-Faffy <faffynaomi@gmail.com>
2025-05-08 17:40:57 +0200

    Updated index.html and added about/contact pages

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function hamburg(){
+function hamburg() {
     const navbar = document.querySelector('.dropdown');
     navbar.style.transform = 'translateY(0px)';
 }
 
-function cancel(){
+function cancel() {
     const navbar = document.querySelector('.dropdown');
     navbar.style.transform = 'translateY(-500px)';
 }
@@ -15,29 +15,25 @@ const texts = [
 ];
 
 let speed = 100;
-
 const textElements = document.querySelector('.typewriter-text');
-
 let textIndex = 0;
 let characterIndex = 0;
 
-function typeWriter(){
-    if(characterIndex < texts[textIndex].length){
+function typeWriter() {
+    if (characterIndex < texts[textIndex].length) {
         textElements.innerHTML += texts[textIndex].charAt(characterIndex);
         characterIndex++;
         setTimeout(typeWriter, speed);
-    }
-    else{
+    } else {
         setTimeout(eraseText, 1000);
     }
 }
 
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
         textElements.innerHTML = textElements.innerHTML.slice(0, -1);
         setTimeout(eraseText, 50);
-    }
-    else{
+    } else {
         textIndex = (textIndex + 1) % texts.length;
         characterIndex = 0;
         setTimeout(typeWriter, 500);
