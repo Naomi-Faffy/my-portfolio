433d2c791f60f2dcbf065426c40fec9cf46bdc5e
Naomi-Faffy <faffynaomi@gmail.com>
2025-05-17 13:01:49 +0200

    Updated to modern minimalist design

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
-function hamburg() {
-    const navbar = document.querySelector('.dropdown');
-    navbar.style.transform = 'translateY(0px)';
+// Simple typewriter effect
+
+const typewriterElement = document.querySelector('.typewriter');
+const text = 'Creating with Passion & Style';
+let idx = 0;
+
+function type() {
+  if (idx < text.length) {
+    typewriterElement.textContent += text.charAt(idx);
+    idx++;
+    setTimeout(type, 100);
+  }
 }
 
-function cancel() {
-    const navbar = document.querySelector('.dropdown');
-    navbar.style.transform = 'translateY(-500px)';
-}
+window.onload = () => {
+  type();
+};
 
-const texts = [
-    "DEVELOPER",
-    "GRAPHIC DESIGNER",
-    "PROGRAMMER"
-];
+// Optional: Add a simple alert on button click
 
-let speed = 100;
-const textElements = document.querySelector('.typewriter-text');
-let textIndex = 0;
-let characterIndex = 0;
-
-function typeWriter() {
-    if (characterIndex < texts[textIndex].length) {
-        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
-        characterIndex++;
-        setTimeout(typeWriter, speed);
-    } else {
-        setTimeout(eraseText, 1000);
-    }
-}
-
-function eraseText() {
-    if (textElements.innerHTML.length > 0) {
-        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
-        setTimeout(eraseText, 50);
-    } else {
-        textIndex = (textIndex + 1) % texts.length;
-        characterIndex = 0;
-        setTimeout(typeWriter, 500);
-    }
-}
-
-window.onload = typeWriter;
+document.getElementById('contactBtn').addEventListener('click', () => {
+  alert('Thanks for your interest! You can reach me at email@example.com');
+});
